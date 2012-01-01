b1f0dc5a7cdfe6b795c0d71693e2a20e67ef9bca
H2CO3 <arpad.goretity@gmail.com>
2012-01-01 22:42:38 +0100

    added sample code and README; updated Makefile

---
+++ Server.m
@@ -0,0 +1,76 @@
+//
+// Server.m
+// Example code for TCPHelper
+//
+// Created by Árpád Goretity on 01/01/2012.
+// Launch me using: ./server <port>
+//
+
+#import <Foundation/Foundation.h>
+#import "TCPHelper.h"
+
+@interface Server: NSObject <TCPHelperDelegate> {
+	TCPHelper *tcpHelper;
+}
+
+- (id) initWithPort:(NSString *)port;
+
+@end
+
+@implementation Server
+
+- (id) initWithPort:(NSString *)port {
+	self = [self init];
+	// host can be nil when it will be used as a server
+	tcpHelper = [[TCPHelper alloc] initWithHost:NULL port:port];
+	tcpHelper.delegate = self;
+	[tcpHelper startServer];
+	return self;
+}
+
+- (void) dealloc {
+	[tcpHelper release];
+	[super dealloc];
+}
+
+// TCPHelperDelegate
+
+- (void) tcpHelperStartedRunning:(TCPHelper *)helper {
+	NSLog(@"Listening on port %@", helper.port);
+}
+
+- (void) tcpHelperConnected:(TCPHelper *)helper {
+	NSLog(@"Connected on port %@, sending data...", helper.port);
+	char *msg = "Hello World!";
+	NSData *data = [NSData dataWithBytes:msg length:strlen(msg)];
+	[helper sendData:data];
+}
+
+- (void) tcpHelper:(TCPHelper *)helper sentData:(NSData *)data {
+	NSLog(@"Data sent! Disconnecting...");
+	[helper disconnect];
+}
+
+- (void) tcpHelperDisconnected:(TCPHelper *)helper {
+	NSLog(@"Disconnected, exiting.");
+	exit(0);
+}
+
+@end
+
+int main(int argc, char **argv) {
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	NSRunLoop *rl = [NSRunLoop currentRunLoop];
+	NSDate *now = [[NSDate alloc] init];
+	// keep the run loop alive
+	NSTimer *tmr = [[NSTimer alloc] initWithFireDate:now interval:60.0 target:NULL selector:NULL userInfo:NULL repeats:YES];
+	[now release];
+	[rl addTimer:tmr forMode:NSDefaultRunLoopMode];
+	[tmr release];
+	NSString *port = [NSString stringWithUTF8String:argv[1]]; // ./server 5555
+	Server *server = [[Server alloc] initWithPort:port];
+	[rl run];
+	[pool release];
+	return 0;
+}
+
